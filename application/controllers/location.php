0c7d7ede54b5e43a534fcff363531e243f803aaf
parksangwon <ourtree4@gmail.com>
2015-08-12 21:24:10 +0900

    [Update] location 수정, emerg 임시 삭제
    date 링크로 전달받도록 수정하고 테이블 속성 datatime->date로 수정

--- application/controllers/location.php
+++ application/controllers/location.php
@@ -3,23 +3,22 @@ class Location extends CI_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->database();
-		$this->load->model('location_model');
+		$this->load->model('Location_model');
 	}
 	/**
 	insert method
 	URL로 입력받은 데이터를 DB에 저장하고 성공 : 0 , 실패 : 1 json으로 return
-	link : http://(serverIP)/everywear/index.php/insert?ino=(data)&lon=(data)&lat=(data)
+	link : http://(serverIP)/index.php/Location/insert_locaton?ino=(data)&date=(date)&lon=(data)&lat=(data)
 	*/
-	function insert() {
-		// validation?
+	function insert_location() {
 
-		$result = $this->location_model->add(array(
-			'ino'=>$this->input->get('ino'),
-			'lon'=>$this->input->get('lon'),
-			'lat'=>$this->input->get('lat')
+		$result = $this->Location_model->add(array(
+			'ino'	=>$this->input->get('ino'),
+			'date'	=>$this->input->get('date'),
+			'lon'	=>$this->input->get('lon'),
+			'lat'	=>$this->input->get('lat')
 		));
-		#echo var_dump($result); #성공시 리턴값 auto_increment 값 # 실패시 null
-
+		#성공시 auto_increment 값, 실패시 null
 		if ( $result != NULL ) {
 			$json_data = array(
 				'result' => 0
@@ -33,9 +32,7 @@ class Location extends CI_Controller {
 		echo $json_data;
 	}
 	function gets() {
-		$this->load->database();
-		$this->load->model('location_model');
-		$data = $this->location_model->gets();
+		$data = $this->Location_model->gets();
 	}
 }
 ?>
--- application/models/location_model.php
+++ application/models/location_model.php
@@ -1,20 +1,21 @@
 <?php
 class Location_model extends CI_Model {
 	function __construct(){
-		parent::__construct();
+
 	}
 	/**
 	add 메서드
-	[name, time, lon, lat] 데이터를 갖는 Array를 인자로 $data에 넘겨 받고,
+	[name, date, lon, lat] 데이터를 갖는 Array를 인자로 $data에 넘겨 받고,
 	각 항복을 setting한 후 location이라는 테이블에 Insert한다.
 	성공 시, AUTO_INCREMENT 값 Return
 	*/
 	function add($data) {
 		//echo var_dump($data);
 		$this->db->set('ino', $data['ino']);
+		$this->db->set('date', $data['date']);
 		$this->db->set('lon', $data['lon']);
 		$this->db->set('lat', $data['lat']);
-		$this->db->insert('location');
+		$this->db->insert('LOCATION');
 		$result = $this->db->insert_id(); // auto_increment value
 		//echo $this->db->last_query(); 마지막 성공쿼리
 		//echo $result;
