a05976caaf79ddc64f5288059a564406f6a8b68f
sangwon <ourtree4@gmail.com>
2015-08-22 14:37:45 +0900

    [modify] Location_model 존재하지 않는 일련번호일때 0리턴
    ino라는 속성이 외래키이므로 validation이 필요하다.

--- application/models/Location_model.php
+++ application/models/Location_model.php
@@ -7,27 +7,38 @@ class Location_model extends CI_Model {
 	addLocation method
 	[name, date, lon, lat] 데이터를 갖는 Array를 인자로 $data에 넘겨 받고,
 	각 항복을 setting한 후 location이라는 테이블에 Insert한다.
-	성공 시 1, 실패 시 0
+	성공 시 1, 실패, 존재하지 않는 일련번호 일 때 0
 	*/
 	function addLocation($data) {
-		//echo var_dump($data);
-		$this->db->set('ino', $data['ino']);
-		$this->db->set('date', $data['date']);
-		$this->db->set('lon', $data['lon']);
-		$this->db->set('lat', $data['lat']);
-		$this->db->insert('LOCATION');
-		$result = $this->db->insert_id(); // auto_increment value
+
+		/* 존재하는 디바이스인지 먼저 validation */
+		$this->load->model('Usercheck_model');
+		$device = $this->Usercheck_model->searchDevice($data['ino']);
 
 		$json_data = null;
-		//성공시 auto_increment 값, 실패시 null
-		if ( $result != NULL ) {
-			$json_data = array(
-				'result' => 1
-			);
-		}  else {
+		if ($device['result'] == 0) {
 			$json_data = array(
 				'result' => 0
 			);
+		}
+		else {
+			$this->db->set('ino', $data['ino']);
+			$this->db->set('date', $data['date']);
+			$this->db->set('lon', $data['lon']);
+			$this->db->set('lat', $data['lat']);
+			$this->db->insert('LOCATION');
+			$result = $this->db->insert_id(); // auto_increment value
+
+			//성공시 auto_increment 값, 실패시 null
+			if ( $result != NULL ) {
+				$json_data = array(
+					'result' => 1
+				);
+			}  else {
+				$json_data = array(
+					'result' => 0
+				);
+			}
 		}
 		return $json_data;
 	}
