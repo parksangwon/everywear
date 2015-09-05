f0f7d69eb019872adf822e19854d6d8edf71408c
hjh112 <hjh112@naver.com>
2015-09-05 17:25:54 +0900

    pw변경

---
+++ application/controllers/Pwchange.php
@@ -0,0 +1,54 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access aloowed');
+class Pwchange  extends CI_Controller{
+	function __construct() {
+		parent::__construct();
+		$this->load->database();
+		$this->load->model('Pwchange_model');
+	}
+
+
+/**
+check 메소드는 링크로부터 id와 기존의 pw를 받아와서 
+check_model로 id와 pw를 매개변수로 전달한다.
+이후 result로 돌려받은 값이 1이면 DB의 데이터와 링크로 받은 pw가 같다는 것을 의미하기때문에
+id와 변경할 비밀번호 change_pw 변수를 change메서드로 넘긴다.
+만약 result의 값이 0이면 DB의 pw와 링크를 통해 받은 pw값이 다름을 의미함.
+result로 0을 어플에 반환한다.
+*/
+	function check(){
+		$id=$this->input->get('id');
+		$pw=$this->input->get('pw');
+		$change_pw=$this->input->get('changePw');
+
+		$result=$this->Pwchange_model->check($id,$pw);
+			 
+			if($result['result']==1){
+				$this-> change($id,$change_pw);
+			}else{
+
+				echo json_encode($result);
+			}
+
+
+
+	}
+/**
+ change 메소드는 check메서드로부터 id와 change_pw를 매개변수로 넘겨받아서
+ 비밀번호를 변경하기 위해서 model로 넘기는 역할을 한다. 
+*/
+	function change($id,$change_pw){
+		
+		$result=$this->Pwchange_model->change($id,$change_pw);
+
+		echo json_encode($result);
+
+	}	
+
+}
+
+
+
+
+
+
+?>
---
+++ application/models/Pwchange_model.php
@@ -0,0 +1,59 @@
+<?php
+class Pwchange_model extends CI_Model{
+	function __construct(){
+		parent::__construct();
+	}
+
+/**
+check메소드는 controller로 부터 넘겨받은 매개변수 id와 pw로
+DB에 저장된 pw와 링크를 통해 받은 pw가 같은지를 체크하는 메소드이다.
+DB에 저장된 pw와 매개변수로 받은 pw가 같으면 1을 반환한다. (아니면 0을 반환)
+*/
+	function check($id,$pw){
+		$query="SELECT pw FROM APPUSER WHERE id='$id'";
+		$query_result= $this->db ->query($query)->row();
+	
+		$json_data=null;
+		if ( 0 == strcmp($query_result->pw, $pw) ) {
+			$json_data=array(
+				'result'=>1);
+		} else{
+			$json_data = array(
+				'result' =>0);
+		}
+		return ($json_data);
+	}
+
+/**
+model의 change 메소드는 id와 변경할 비밀번호 change_pw를 넘겨받고
+DB에 저장된 비밀번호를 변경한 후,
+변경이 성공적으로 이루어지면 (query_result==true) result의 값을 1, 아니면 0을 반환
+*/
+	function change($id,$change_pw){
+		$query="UPDATE APPUSER SET pw='$change_pw' WHERE id='$id'";
+		$query_result=$this->db->query($query);
+	
+
+		$json_data=null;
+		if($query_result==TRUE){
+
+			$json_data=array(
+				'result'=>1
+			);
+		}else{
+			$json_data=array(
+				'result'=>0
+			);
+		}
+
+		return $json_data;
+	}
+
+	
+
+
+}
+
+
+
+?>
