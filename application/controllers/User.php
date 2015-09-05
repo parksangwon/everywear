a2ad47d45d0c7922d3773a1e59b1a44c900abfa8
sangwon <ourtree4@gmail.com>
2015-09-05 15:48:04 +0900

    [ㅇㅇㅇ]
    ㅇㅇ

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,6 +5,19 @@ class User  extends CI_Controller{
 		$this->load->database();
 		$this->load->model('User_model');
 	}
+
+	/**
+	validId method
+	id와 일치하는 id가 있는지 확인. 존재하면 1, 존재하지 않으면 0
+	link : http://(serverIP)/index.php/User/checkDevice?ino=(ino)
+	*/
+	function validId() {
+		$id = $this->input->get('id');
+		$result = $this->User_model->isExsist($id);
+		$result = $this->toArray($result);
+		echo json_encode($result);
+	}
+	
 	/**
 	checkId method
 	id와 pw를 입력받고 DB에서 조회한다. 일치하면 1과 이름, 존재하지 않거나 일치하지 않으면 0
@@ -24,18 +37,6 @@ class User  extends CI_Controller{
 	function checkDevice() {
 		$ino = $this->input->get('ino');
 		$result = $this->User_model->searchDevice($ino);
-		echo json_encode($result);
-	}
-
-	/**
-	validId method
-	id와 일치하는 id가 있는지 확인. 존재하면 1, 존재하지 않으면 0
-	link : http://(serverIP)/index.php/User/checkDevice?ino=(ino)
-	*/
-	function validId() {
-		$id = $this->input->get('id');
-		$result = $this->User_model->isExsist($id);
-		$result = $this->toArray($result);
 		echo json_encode($result);
 	}
 
@@ -66,14 +67,15 @@ class User  extends CI_Controller{
 		echo json_encode($result);
 	}
 
-	private function toArray($bool) {
-		$json_data = array('result' => 0);
-		if ( $bool == TRUE ) {
-			$json_data = array(
-				'result' => 1
-			);
+	// 개인정보 조회
+	function checkInfo() {
+		$id = urldecode( $this->input->get('id') );
+
+		$result = array('result'=>0);
+		if ( strcmp("", $id) != 0 ) {
+			$result = $this->User_model->lookForUser($id);
 		}
-		return $json_data;
+		echo urldecode(json_encode($result));
 	}
 }
 ?>
